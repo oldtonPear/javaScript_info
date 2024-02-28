55b04a79a8e1d56eeac8aaa5457a838277c7e37b
oldtonPear <christian.torli@itiszuccante.edu.it>
2024-02-28 11:56:17 +0100

    .

--- lista_prodotti_better/script.js
+++ lista_prodotti_better/script.js
@@ -2,26 +2,44 @@ let prodotti = [];
 
 function showProducts() {
     let s = ''
+    let i = 0;
     prodotti.forEach(prodotto => {
-        s += '<div>'+ prodotto.codice + ' ' + prodotto.nome
-        + ' ' + prodotto.descrizione + ' ' + prodotto.prezzo + '</div>'
+        s += '<div style = "margin: 30px">'+ 
+        prodotto.codice 
+        + ' ' + 
+        prodotto.nome
+        + ' ' + 
+        prodotto.descrizione 
+        + ' ' + 
+        prodotto.prezzo 
+        + 
+        '<button type="button" class="btn btn-secondary" style = "float: right" onclick="editProduct('+i+');">Edit</button>' + 
+        '<button type="button" class="btn btn-primary" style = "float: right" onclick="deleteProduct('+i+');">Delete</button>' + 
+        '</div>';
+        i++;
     });
     document.getElementById('modal-content').innerHTML = s
 }
 
 function addProduct(product) {
     prodotti.push(product)
-      showProducts();
+    showProducts();
 }
 
-function deleteProduct() {
-    let s = prompt("Inserisci il codice dell'elemento da rimuovere:");
-    prodotti.forEach(prodotto => {
-        if(prodotto.codice === s){
-            prodotti.splice(prodotti.indexOf(prodotto), 1);
-        }
-    });
+function deleteProduct(pos) {
+    prodotti.splice(prodotti[pos], 1);
     showProducts();
+}
+
+function editProduct(pos){
+    var prodotto = prodotti[pos];
+    const form = document.querySelector('#myForm');
+    form.querySelector('#categoria').value = prodotto.categoria;
+    form.querySelector('#codice').value = prodotto.codice;
+    form.querySelector('#nome').value = prodotto.nome;
+    form.querySelector('#descrizione').value = prodotto.descrizione;
+    form.querySelector('#prezzo').value = prodotto.prezzo;
+    form.querySelector('#inMagazzino').checked = prodotto.inMagazzino;
 }
 
 function sortProducts() {
@@ -44,9 +62,15 @@ function validateMyForm(){
     const form = document.querySelector('#myForm');
 
     var radioButtonGroup = document.getElementsByName("sconto");
-    var radiob = Array.from(radioButtonGroup).find(
-        (radio) => radio.checked
-     );
+    var radiob;
+    try{
+        radiob = Array.from(radioButtonGroup).find(
+            (radio) => radio.checked.value
+         );
+    }catch(error){
+        radiob = null;
+    }
+    
     
     product = {
         categoria: form.querySelector('#categoria').value,
@@ -54,7 +78,7 @@ function validateMyForm(){
         nome: form.querySelector('#nome').value,
         descrizione: form.querySelector('#descrizione').value,
         prezzo: form.querySelector('#prezzo').value,
-        sconto: radiob.value,
+        sconto: radiob,
         inMagazzino: form.querySelector('#inMagazzino').checked
     };
     addProduct(product)
